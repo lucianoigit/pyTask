68ed236b0d8d9e106578248e6929877037c318eb
lucianoigit <lucianoiriartegit@gmail.com>
2024-10-16 23:29:25 -0300

    feat : Aniade informacion de desarrollador al setup"

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tasklib",
+    name="pyTask",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[],
-    author="Tu Nombre",
+    author="Luciano Iriarte",
     description="Librería para ejecutar tareas concurrentes con hilos",
     classifiers=[
         "Programming Language :: Python :: 3",
